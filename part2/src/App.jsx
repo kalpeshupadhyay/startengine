a75e4411e76df6b6dfe78f42f79c41a272bbe3f1
Kalpesh Upadhyay <kalpeshupadhyay@Kalpeshs-MacBook-Pro.local>
2024-09-13 12:56:01 +0530

    second fur

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -3,9 +3,19 @@ import Note from "./components/Note"
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note..')
+  const [showAll, setShowAll] = useState(true)
+
+
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
   const handleNoteChange = (event)=> {
@@ -13,17 +23,21 @@ const App = (props) => {
     setNewNote(event.target.value)
     console.log(event.target.value)
   }
+
+  const notesToShow = showAll
+  ?notes
+  : notes.filter(note=>note.important === true)
   return(
     <div>
       <h1>
         NOTES
       </h1>
       <ul>
-      {notes.map(x=> <Note key={x.id} note={x}/>)}
+      {notesToShow.map(note=> <Note key={note.id} note={note}/>)}
       </ul>
       <form onSubmit={addNote}>
         <input value = {newNote} onChange={handleNoteChange} />
-        <button >Save</button>
+        <button onClick={()=>setShowAll(!showAll)}>show{showAll?'important':'all'}</button>
       </form>
       
     </div>
